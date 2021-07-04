d9c4804e0dc422674f43078a64ae00c2a478a551
damn-clown <86931144+damn-clown@users.noreply.github.com>
2021-07-04 13:37:05 -0700

    Add files via upload

---
+++ height detector.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    int height = 00;
+    std::cout << "How tall are you in cm?";
+    std::cin >> height;
+    std::cout << " you are " << height << " cm tall!";
+}
